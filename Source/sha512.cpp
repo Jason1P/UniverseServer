ba42abbf3a3258a8fecca49c5f20863481af0050
LegoUniverseIon <legouniverseion@gmail.com>
2016-03-30 21:27:05 -0400

    SHA512 Password Hashing

--- Source/sha512.cpp
+++ Source/sha512.cpp
@@ -1,362 +1,155 @@
-/* MD5
-converted to C++ class by Frank Thilo (thilo@unix-ag.org)
-for bzflag (http://www.bzflag.org)
+#include <cstring>
+#include <fstream>
+#include "sha512.h"
 
-based on:
+const unsigned long long SHA512::sha512_k[80] = //ULL = uint64
+{ 0x428a2f98d728ae22ULL, 0x7137449123ef65cdULL,
+0xb5c0fbcfec4d3b2fULL, 0xe9b5dba58189dbbcULL,
+0x3956c25bf348b538ULL, 0x59f111f1b605d019ULL,
+0x923f82a4af194f9bULL, 0xab1c5ed5da6d8118ULL,
+0xd807aa98a3030242ULL, 0x12835b0145706fbeULL,
+0x243185be4ee4b28cULL, 0x550c7dc3d5ffb4e2ULL,
+0x72be5d74f27b896fULL, 0x80deb1fe3b1696b1ULL,
+0x9bdc06a725c71235ULL, 0xc19bf174cf692694ULL,
+0xe49b69c19ef14ad2ULL, 0xefbe4786384f25e3ULL,
+0x0fc19dc68b8cd5b5ULL, 0x240ca1cc77ac9c65ULL,
+0x2de92c6f592b0275ULL, 0x4a7484aa6ea6e483ULL,
+0x5cb0a9dcbd41fbd4ULL, 0x76f988da831153b5ULL,
+0x983e5152ee66dfabULL, 0xa831c66d2db43210ULL,
+0xb00327c898fb213fULL, 0xbf597fc7beef0ee4ULL,
+0xc6e00bf33da88fc2ULL, 0xd5a79147930aa725ULL,
+0x06ca6351e003826fULL, 0x142929670a0e6e70ULL,
+0x27b70a8546d22ffcULL, 0x2e1b21385c26c926ULL,
+0x4d2c6dfc5ac42aedULL, 0x53380d139d95b3dfULL,
+0x650a73548baf63deULL, 0x766a0abb3c77b2a8ULL,
+0x81c2c92e47edaee6ULL, 0x92722c851482353bULL,
+0xa2bfe8a14cf10364ULL, 0xa81a664bbc423001ULL,
+0xc24b8b70d0f89791ULL, 0xc76c51a30654be30ULL,
+0xd192e819d6ef5218ULL, 0xd69906245565a910ULL,
+0xf40e35855771202aULL, 0x106aa07032bbd1b8ULL,
+0x19a4c116b8d2d0c8ULL, 0x1e376c085141ab53ULL,
+0x2748774cdf8eeb99ULL, 0x34b0bcb5e19b48a8ULL,
+0x391c0cb3c5c95a63ULL, 0x4ed8aa4ae3418acbULL,
+0x5b9cca4f7763e373ULL, 0x682e6ff3d6b2b8a3ULL,
+0x748f82ee5defb2fcULL, 0x78a5636f43172f60ULL,
+0x84c87814a1f0ab72ULL, 0x8cc702081a6439ecULL,
+0x90befffa23631e28ULL, 0xa4506cebde82bde9ULL,
+0xbef9a3f7b2c67915ULL, 0xc67178f2e372532bULL,
+0xca273eceea26619cULL, 0xd186b8c721c0c207ULL,
+0xeada7dd6cde0eb1eULL, 0xf57d4f7fee6ed178ULL,
+0x06f067aa72176fbaULL, 0x0a637dc5a2c898a6ULL,
+0x113f9804bef90daeULL, 0x1b710b35131c471bULL,
+0x28db77f523047d84ULL, 0x32caab7b40c72493ULL,
+0x3c9ebe0a15c9bebcULL, 0x431d67c49c100d4cULL,
+0x4cc5d4becb3e42b6ULL, 0x597f299cfc657e2aULL,
+0x5fcb6fab3ad6faecULL, 0x6c44198c4a475817ULL };
 
-md5.h and md5.c
-reference implemantion of RFC 1321
-
-Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
-rights reserved.
-
-License to copy and use this software is granted provided that it
-is identified as the "RSA Data Security, Inc. MD5 Message-Digest
-Algorithm" in all material mentioning or referencing this software
-or this function.
-
-License is also granted to make and use derivative works provided
-that such works are identified as "derived from the RSA Data
-Security, Inc. MD5 Message-Digest Algorithm" in all material
-mentioning or referencing the derived work.
-
-RSA Data Security, Inc. makes no representations concerning either
-the merchantability of this software or the suitability of this
-software for any particular purpose. It is provided "as is"
-without express or implied warranty of any kind.
-
-These notices must be retained in any copies of any part of this
-documentation and/or software.
-
-*/
-
-/* interface header */
-#include "md5.h"
-
-/* system implementation headers */
-#include <cstdio>
-
-
-// Constants for MD5Transform routine.
-#define S11 7
-#define S12 12
-#define S13 17
-#define S14 22
-#define S21 5
-#define S22 9
-#define S23 14
-#define S24 20
-#define S31 4
-#define S32 11
-#define S33 16
-#define S34 23
-#define S41 6
-#define S42 10
-#define S43 15
-#define S44 21
-
-///////////////////////////////////////////////
-
-// F, G, H and I are basic MD5 functions.
-inline MD5::uint4 MD5::F(uint4 x, uint4 y, uint4 z) {
-	return x&y | ~x&z;
-}
-
-inline MD5::uint4 MD5::G(uint4 x, uint4 y, uint4 z) {
-	return x&z | y&~z;
-}
-
-inline MD5::uint4 MD5::H(uint4 x, uint4 y, uint4 z) {
-	return x^y^z;
-}
-
-inline MD5::uint4 MD5::I(uint4 x, uint4 y, uint4 z) {
-	return y ^ (x | ~z);
-}
-
-// rotate_left rotates x left n bits.
-inline MD5::uint4 MD5::rotate_left(uint4 x, int n) {
-	return (x << n) | (x >> (32 - n));
-}
-
-// FF, GG, HH, and II transformations for rounds 1, 2, 3, and 4.
-// Rotation is separate from addition to prevent recomputation.
-inline void MD5::FF(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac) {
-	a = rotate_left(a + F(b, c, d) + x + ac, s) + b;
-}
-
-inline void MD5::GG(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac) {
-	a = rotate_left(a + G(b, c, d) + x + ac, s) + b;
-}
-
-inline void MD5::HH(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac) {
-	a = rotate_left(a + H(b, c, d) + x + ac, s) + b;
-}
-
-inline void MD5::II(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac) {
-	a = rotate_left(a + I(b, c, d) + x + ac, s) + b;
-}
-
-//////////////////////////////////////////////
-
-// default ctor, just initailize
-MD5::MD5()
+void SHA512::transform(const unsigned char *message, unsigned int block_nb)
 {
-	init();
-}
+	uint64 w[80];
+	uint64 wv[8];
+	uint64 t1, t2;
+	const unsigned char *sub_block;
+	int i, j;
+	for (i = 0; i < (int)block_nb; i++) {
+		sub_block = message + (i << 7);
+		for (j = 0; j < 16; j++) {
+			SHA2_PACK64(&sub_block[j << 3], &w[j]);
+		}
+		for (j = 16; j < 80; j++) {
+			w[j] = SHA512_F4(w[j - 2]) + w[j - 7] + SHA512_F3(w[j - 15]) + w[j - 16];
+		}
+		for (j = 0; j < 8; j++) {
+			wv[j] = m_h[j];
+		}
+		for (j = 0; j < 80; j++) {
+			t1 = wv[7] + SHA512_F2(wv[4]) + SHA2_CH(wv[4], wv[5], wv[6])
+				+ sha512_k[j] + w[j];
+			t2 = SHA512_F1(wv[0]) + SHA2_MAJ(wv[0], wv[1], wv[2]);
+			wv[7] = wv[6];
+			wv[6] = wv[5];
+			wv[5] = wv[4];
+			wv[4] = wv[3] + t1;
+			wv[3] = wv[2];
+			wv[2] = wv[1];
+			wv[1] = wv[0];
+			wv[0] = t1 + t2;
+		}
+		for (j = 0; j < 8; j++) {
+			m_h[j] += wv[j];
+		}
 
-//////////////////////////////////////////////
-
-// nifty shortcut ctor, compute MD5 for string and finalize it right away
-MD5::MD5(const std::string &text)
-{
-	init();
-	update(text.c_str(), text.length());
-	finalize();
-}
-
-//////////////////////////////
-
-void MD5::init()
-{
-	finalized = false;
-
-	count[0] = 0;
-	count[1] = 0;
-
-	// load magic initialization constants.
-	state[0] = 0x67452301;
-	state[1] = 0xefcdab89;
-	state[2] = 0x98badcfe;
-	state[3] = 0x10325476;
-}
-
-//////////////////////////////
-
-// decodes input (unsigned char) into output (uint4). Assumes len is a multiple of 4.
-void MD5::decode(uint4 output[], const uint1 input[], size_type len)
-{
-	for (unsigned int i = 0, j = 0; j < len; i++, j += 4)
-		output[i] = ((uint4)input[j]) | (((uint4)input[j + 1]) << 8) |
-		(((uint4)input[j + 2]) << 16) | (((uint4)input[j + 3]) << 24);
-}
-
-//////////////////////////////
-
-// encodes input (uint4) into output (unsigned char). Assumes len is
-// a multiple of 4.
-void MD5::encode(uint1 output[], const uint4 input[], size_type len)
-{
-	for (size_type i = 0, j = 0; j < len; i++, j += 4) {
-		output[j] = input[i] & 0xff;
-		output[j + 1] = (input[i] >> 8) & 0xff;
-		output[j + 2] = (input[i] >> 16) & 0xff;
-		output[j + 3] = (input[i] >> 24) & 0xff;
 	}
 }
 
-//////////////////////////////
-
-// apply MD5 algo on a block
-void MD5::transform(const uint1 block[blocksize])
+void SHA512::init()
 {
-	uint4 a = state[0], b = state[1], c = state[2], d = state[3], x[16];
-	decode(x, block, blocksize);
-
-	/* Round 1 */
-	FF(a, b, c, d, x[0], S11, 0xd76aa478); /* 1 */
-	FF(d, a, b, c, x[1], S12, 0xe8c7b756); /* 2 */
-	FF(c, d, a, b, x[2], S13, 0x242070db); /* 3 */
-	FF(b, c, d, a, x[3], S14, 0xc1bdceee); /* 4 */
-	FF(a, b, c, d, x[4], S11, 0xf57c0faf); /* 5 */
-	FF(d, a, b, c, x[5], S12, 0x4787c62a); /* 6 */
-	FF(c, d, a, b, x[6], S13, 0xa8304613); /* 7 */
-	FF(b, c, d, a, x[7], S14, 0xfd469501); /* 8 */
-	FF(a, b, c, d, x[8], S11, 0x698098d8); /* 9 */
-	FF(d, a, b, c, x[9], S12, 0x8b44f7af); /* 10 */
-	FF(c, d, a, b, x[10], S13, 0xffff5bb1); /* 11 */
-	FF(b, c, d, a, x[11], S14, 0x895cd7be); /* 12 */
-	FF(a, b, c, d, x[12], S11, 0x6b901122); /* 13 */
-	FF(d, a, b, c, x[13], S12, 0xfd987193); /* 14 */
-	FF(c, d, a, b, x[14], S13, 0xa679438e); /* 15 */
-	FF(b, c, d, a, x[15], S14, 0x49b40821); /* 16 */
-
-	/* Round 2 */
-	GG(a, b, c, d, x[1], S21, 0xf61e2562); /* 17 */
-	GG(d, a, b, c, x[6], S22, 0xc040b340); /* 18 */
-	GG(c, d, a, b, x[11], S23, 0x265e5a51); /* 19 */
-	GG(b, c, d, a, x[0], S24, 0xe9b6c7aa); /* 20 */
-	GG(a, b, c, d, x[5], S21, 0xd62f105d); /* 21 */
-	GG(d, a, b, c, x[10], S22, 0x2441453); /* 22 */
-	GG(c, d, a, b, x[15], S23, 0xd8a1e681); /* 23 */
-	GG(b, c, d, a, x[4], S24, 0xe7d3fbc8); /* 24 */
-	GG(a, b, c, d, x[9], S21, 0x21e1cde6); /* 25 */
-	GG(d, a, b, c, x[14], S22, 0xc33707d6); /* 26 */
-	GG(c, d, a, b, x[3], S23, 0xf4d50d87); /* 27 */
-	GG(b, c, d, a, x[8], S24, 0x455a14ed); /* 28 */
-	GG(a, b, c, d, x[13], S21, 0xa9e3e905); /* 29 */
-	GG(d, a, b, c, x[2], S22, 0xfcefa3f8); /* 30 */
-	GG(c, d, a, b, x[7], S23, 0x676f02d9); /* 31 */
-	GG(b, c, d, a, x[12], S24, 0x8d2a4c8a); /* 32 */
-
-	/* Round 3 */
-	HH(a, b, c, d, x[5], S31, 0xfffa3942); /* 33 */
-	HH(d, a, b, c, x[8], S32, 0x8771f681); /* 34 */
-	HH(c, d, a, b, x[11], S33, 0x6d9d6122); /* 35 */
-	HH(b, c, d, a, x[14], S34, 0xfde5380c); /* 36 */
-	HH(a, b, c, d, x[1], S31, 0xa4beea44); /* 37 */
-	HH(d, a, b, c, x[4], S32, 0x4bdecfa9); /* 38 */
-	HH(c, d, a, b, x[7], S33, 0xf6bb4b60); /* 39 */
-	HH(b, c, d, a, x[10], S34, 0xbebfbc70); /* 40 */
-	HH(a, b, c, d, x[13], S31, 0x289b7ec6); /* 41 */
-	HH(d, a, b, c, x[0], S32, 0xeaa127fa); /* 42 */
-	HH(c, d, a, b, x[3], S33, 0xd4ef3085); /* 43 */
-	HH(b, c, d, a, x[6], S34, 0x4881d05); /* 44 */
-	HH(a, b, c, d, x[9], S31, 0xd9d4d039); /* 45 */
-	HH(d, a, b, c, x[12], S32, 0xe6db99e5); /* 46 */
-	HH(c, d, a, b, x[15], S33, 0x1fa27cf8); /* 47 */
-	HH(b, c, d, a, x[2], S34, 0xc4ac5665); /* 48 */
-
-	/* Round 4 */
-	II(a, b, c, d, x[0], S41, 0xf4292244); /* 49 */
-	II(d, a, b, c, x[7], S42, 0x432aff97); /* 50 */
-	II(c, d, a, b, x[14], S43, 0xab9423a7); /* 51 */
-	II(b, c, d, a, x[5], S44, 0xfc93a039); /* 52 */
-	II(a, b, c, d, x[12], S41, 0x655b59c3); /* 53 */
-	II(d, a, b, c, x[3], S42, 0x8f0ccc92); /* 54 */
-	II(c, d, a, b, x[10], S43, 0xffeff47d); /* 55 */
-	II(b, c, d, a, x[1], S44, 0x85845dd1); /* 56 */
-	II(a, b, c, d, x[8], S41, 0x6fa87e4f); /* 57 */
-	II(d, a, b, c, x[15], S42, 0xfe2ce6e0); /* 58 */
-	II(c, d, a, b, x[6], S43, 0xa3014314); /* 59 */
-	II(b, c, d, a, x[13], S44, 0x4e0811a1); /* 60 */
-	II(a, b, c, d, x[4], S41, 0xf7537e82); /* 61 */
-	II(d, a, b, c, x[11], S42, 0xbd3af235); /* 62 */
-	II(c, d, a, b, x[2], S43, 0x2ad7d2bb); /* 63 */
-	II(b, c, d, a, x[9], S44, 0xeb86d391); /* 64 */
-
-	state[0] += a;
-	state[1] += b;
-	state[2] += c;
-	state[3] += d;
-
-	// Zeroize sensitive information.
-	memset(x, 0, sizeof x);
+	m_h[0] = 0x6a09e667f3bcc908ULL;
+	m_h[1] = 0xbb67ae8584caa73bULL;
+	m_h[2] = 0x3c6ef372fe94f82bULL;
+	m_h[3] = 0xa54ff53a5f1d36f1ULL;
+	m_h[4] = 0x510e527fade682d1ULL;
+	m_h[5] = 0x9b05688c2b3e6c1fULL;
+	m_h[6] = 0x1f83d9abfb41bd6bULL;
+	m_h[7] = 0x5be0cd19137e2179ULL;
+	m_len = 0;
+	m_tot_len = 0;
 }
 
-//////////////////////////////
-
-// MD5 block update operation. Continues an MD5 message-digest
-// operation, processing another message block
-void MD5::update(const unsigned char input[], size_type length)
+void SHA512::update(const unsigned char *message, unsigned int len)
 {
-	// compute number of bytes mod 64
-	size_type index = count[0] / 8 % blocksize;
-
-	// Update number of bits
-	if ((count[0] += (length << 3)) < (length << 3))
-		count[1]++;
-	count[1] += (length >> 29);
-
-	// number of bytes we need to fill in buffer
-	size_type firstpart = 64 - index;
-
-	size_type i;
-
-	// transform as many times as possible.
-	if (length >= firstpart)
-	{
-		// fill buffer first, transform
-		memcpy(&buffer[index], input, firstpart);
-		transform(buffer);
-
-		// transform chunks of blocksize (64 bytes)
-		for (i = firstpart; i + blocksize <= length; i += blocksize)
-			transform(&input[i]);
-
-		index = 0;
+	unsigned int block_nb;
+	unsigned int new_len, rem_len, tmp_len;
+	const unsigned char *shifted_message;
+	tmp_len = SHA384_512_BLOCK_SIZE - m_len;
+	rem_len = len < tmp_len ? len : tmp_len;
+	memcpy(&m_block[m_len], message, rem_len);
+	if (m_len + len < SHA384_512_BLOCK_SIZE) {
+		m_len += len;
+		return;
 	}
-	else
-		i = 0;
-
-	// buffer remaining input
-	memcpy(&buffer[index], &input[i], length - i);
+	new_len = len - rem_len;
+	block_nb = new_len / SHA384_512_BLOCK_SIZE;
+	shifted_message = message + rem_len;
+	transform(m_block, 1);
+	transform(shifted_message, block_nb);
+	rem_len = new_len % SHA384_512_BLOCK_SIZE;
+	memcpy(m_block, &shifted_message[block_nb << 7], rem_len);
+	m_len = rem_len;
+	m_tot_len += (block_nb + 1) << 7;
 }
 
-//////////////////////////////
-
-// for convenience provide a verson with signed char
-void MD5::update(const char input[], size_type length)
+void SHA512::final(unsigned char *digest)
 {
-	update((const unsigned char*)input, length);
-}
-
-//////////////////////////////
-
-// MD5 finalization. Ends an MD5 message-digest operation, writing the
-// the message digest and zeroizing the context.
-MD5& MD5::finalize()
-{
-	static unsigned char padding[64] = {
-		0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-	};
-
-	if (!finalized) {
-		// Save number of bits
-		unsigned char bits[8];
-		encode(bits, count, 8);
-
-		// pad out to 56 mod 64.
-		size_type index = count[0] / 8 % 64;
-		size_type padLen = (index < 56) ? (56 - index) : (120 - index);
-		update(padding, padLen);
-
-		// Append length (before padding)
-		update(bits, 8);
-
-		// Store state in digest
-		encode(digest, state, 16);
-
-		// Zeroize sensitive information.
-		memset(buffer, 0, sizeof buffer);
-		memset(count, 0, sizeof count);
-
-		finalized = true;
+	unsigned int block_nb;
+	unsigned int pm_len;
+	unsigned int len_b;
+	int i;
+	block_nb = 1 + ((SHA384_512_BLOCK_SIZE - 17)
+		< (m_len % SHA384_512_BLOCK_SIZE));
+	len_b = (m_tot_len + m_len) << 3;
+	pm_len = block_nb << 7;
+	memset(m_block + m_len, 0, pm_len - m_len);
+	m_block[m_len] = 0x80;
+	SHA2_UNPACK32(len_b, m_block + pm_len - 4);
+	transform(m_block, block_nb);
+	for (i = 0; i < 8; i++) {
+		SHA2_UNPACK64(m_h[i], &digest[i << 3]);
 	}
-
-	return *this;
 }
 
-//////////////////////////////
-
-// return hex representation of digest as string
-std::string MD5::hexdigest() const
+std::string sha512(std::string input)
 {
-	if (!finalized)
-		return "";
+	unsigned char digest[SHA512::DIGEST_SIZE];
+	memset(digest, 0, SHA512::DIGEST_SIZE);
+	SHA512 ctx = SHA512();
+	ctx.init();
+	ctx.update((unsigned char*)input.c_str(), input.length());
+	ctx.final(digest);
 
-	char buf[33];
-	for (int i = 0; i<16; i++)
+	char buf[2 * SHA512::DIGEST_SIZE + 1];
+	buf[2 * SHA512::DIGEST_SIZE] = 0;
+	for (int i = 0; i < SHA512::DIGEST_SIZE; i++)
 		sprintf(buf + i * 2, "%02x", digest[i]);
-	buf[32] = 0;
-
 	return std::string(buf);
-}
-
-//////////////////////////////
-
-std::ostream& operator<<(std::ostream& out, MD5 md5)
-{
-	return out << md5.hexdigest();
-}
-
-//////////////////////////////
-
-std::string md5(const std::string str)
-{
-	MD5 md5 = MD5(str);
-
-	return md5.hexdigest();
 }
